8a84e8e61269019e5f7732635455c2d1bb5522be
Georgios Andreadis <info@gandreadis.com>
2020-09-24 12:41:16 +0200

    Fix social button positioning

--- src/sections/ContactSection.js
+++ src/sections/ContactSection.js
@@ -20,16 +20,17 @@ const ContactSection = ({ intl }) => (
       href="https://www.instagram.com/ensemblezoef/"
       target="_blank"
       rel="noreferrer"
-      className="mr-2 btn btn-light text-primary"
+      className="mr-md-2 btn btn-light text-primary"
     >
       <FaInstagram size={32} className="mr-2" />
       Instagram
     </a>
+    <br className="d-block d-md-none"/>
     <a
       href="https://www.facebook.com/ensemblezoef/"
       target="_blank"
       rel="noreferrer"
-      className="mr-2 btn btn-light text-primary"
+      className="mr-2 mt-2 mt-md-0 btn btn-light text-primary"
     >
       <FaFacebook size={32} className="mr-2" />
       Facebook
@@ -38,7 +39,7 @@ const ContactSection = ({ intl }) => (
       href="https://www.youtube.com/channel/UC-Jw2XCI6tugO3A1KXQ_AxQ"
       target="_blank"
       rel="noreferrer"
-      className="mr-2 btn btn-light text-primary"
+      className="mr-md-2 mt-2 mt-md-0 btn btn-light text-primary"
     >
       <FaYoutube size={32} className="mr-2" />
       YouTube
